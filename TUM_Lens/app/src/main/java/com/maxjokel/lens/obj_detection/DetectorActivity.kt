d9259dc7a271853803010e0c1ab59e010b4bfb27
David Drews <david@drews.io>
2021-06-09 07:09:48 +0200

    Add contents of Android object detection demo to kickstart detector implementation
    https://github.com/tensorflow/examples/tree/master/lite/examples/object_detection/android

---
+++ TUM_Lens/app/src/main/java/com/maxjokel/lens/obj_detection/DetectorActivity.kt
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.maxjokel.lens.obj_detection
+
+import android.graphics.*
+import android.media.ImageReader
+import android.os.SystemClock
+import android.util.Size
+import android.util.TypedValue
+import android.view.View
+import android.widget.Toast
+import com.maxjokel.lens.helpers.ImageUtils.getTransformationMatrix
+import com.maxjokel.lens.helpers.ImageUtils.saveBitmap
+import com.maxjokel.lens.helpers.Logger
+import java.io.IOException
+import java.util.*
+import com.maxjokel.lens.R
+
+/**
+ * An activity that uses a TensorFlowMultiBoxDetector and ObjectTracker to detect and then track
+ * objects.
+ */
+class DetectorActivity : CameraActivity(), ImageReader.OnImageAvailableListener {
+    private var trackingOverlay: OverlayView? = null
+    private var sensorOrientation: Int? = null
+    private var detector: Detector? = null
+    private var lastProcessingTimeMs: Long = 0
+    private var rgbFrameBitmap: Bitmap? = null
+    private var croppedBitmap: Bitmap? = null
+    private var cropCopyBitmap: Bitmap? = null
+    private var computingDetection = false
+    private var timestamp: Long = 0
+    private var frameToCropTransform: Matrix? = null
+    private var cropToFrameTransform: Matrix? = null
+    private var tracker: MultiBoxTracker? = null
+    private var borderedText: BorderedText? = null
+    public override fun onPreviewSizeChosen(size: Size, rotation: Int) {
+        val textSizePx = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, resources.displayMetrics
+        )
+        borderedText = BorderedText(textSizePx)
+        borderedText!!.setTypeface(Typeface.MONOSPACE)
+        tracker = MultiBoxTracker(this)
+        var cropSize = TF_OD_API_INPUT_SIZE
+        try {
+            detector = TFLiteObjectDetectionAPIModel.create(
+                this,
+                TF_OD_API_MODEL_FILE,
+                TF_OD_API_LABELS_FILE,
+                TF_OD_API_INPUT_SIZE,
+                TF_OD_API_IS_QUANTIZED
+            )
+            cropSize = TF_OD_API_INPUT_SIZE
+        } catch (e: IOException) {
+            e.printStackTrace()
+            LOGGER.e(e, "Exception initializing Detector!")
+            val toast = Toast.makeText(
+                applicationContext, "Detector could not be initialized", Toast.LENGTH_SHORT
+            )
+            toast.show()
+            finish()
+        }
+        previewWidth = size.width
+        previewHeight = size.height
+        sensorOrientation = rotation - screenOrientation
+        LOGGER.i("Camera orientation relative to screen canvas: %d", sensorOrientation)
+        LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight)
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888)
+        croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Bitmap.Config.ARGB_8888)
+        frameToCropTransform = getTransformationMatrix(
+            previewWidth, previewHeight,
+            cropSize, cropSize,
+            sensorOrientation!!, MAINTAIN_ASPECT
+        )
+        cropToFrameTransform = Matrix()
+        frameToCropTransform!!.invert(cropToFrameTransform)
+        trackingOverlay = findViewById<View>(R.id.tracking_overlay) as OverlayView
+        trackingOverlay!!.addCallback { canvas ->
+            tracker!!.draw(canvas)
+            if (isDebug) {
+                tracker!!.drawDebug(canvas)
+            }
+        }
+        tracker!!.setFrameConfiguration(previewWidth, previewHeight, sensorOrientation!!)
+    }
+
+    override fun processImage() {
+        ++timestamp
+        val currTimestamp = timestamp
+        trackingOverlay!!.postInvalidate()
+
+        // No mutex needed as this method is not reentrant.
+        if (computingDetection) {
+            readyForNextImage()
+            return
+        }
+        computingDetection = true
+        LOGGER.i("Preparing image $currTimestamp for detection in bg thread.")
+        rgbFrameBitmap!!.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight)
+        readyForNextImage()
+        val canvas = Canvas(croppedBitmap!!)
+        canvas.drawBitmap(rgbFrameBitmap!!, frameToCropTransform!!, null)
+        // For examining the actual TF input.
+        if (SAVE_PREVIEW_BITMAP) {
+            saveBitmap(croppedBitmap!!)
+        }
+        runInBackground {
+            LOGGER.i("Running detection on image $currTimestamp")
+            val startTime = SystemClock.uptimeMillis()
+            val results = detector!!.recognizeImage(croppedBitmap)
+            lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime
+            cropCopyBitmap = Bitmap.createBitmap(croppedBitmap!!)
+            val tmp_bitmap = cropCopyBitmap
+            val canvas = if (tmp_bitmap != null) {
+                Canvas(tmp_bitmap)
+            } else {
+                null
+            }
+            val paint = Paint()
+            paint.color = Color.RED
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = 2.0f
+            var minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API
+            minimumConfidence = when (MODE) {
+                DetectorMode.TF_OD_API -> MINIMUM_CONFIDENCE_TF_OD_API
+            }
+            val mappedRecognitions: MutableList<Detector.Recognition> = ArrayList()
+            for (result in results) {
+                val location = result.location
+                if (location != null && result.confidence >= minimumConfidence) {
+                    canvas?.drawRect(location, paint)
+                    cropToFrameTransform!!.mapRect(location)
+                    result.location = location
+                    mappedRecognitions.add(result)
+                }
+            }
+            tracker!!.trackResults(mappedRecognitions, currTimestamp)
+            trackingOverlay!!.postInvalidate()
+            computingDetection = false
+            runOnUiThread {
+                showFrameInfo(previewWidth.toString() + "x" + previewHeight)
+                if (canvas != null) {
+                    showCropInfo(canvas?.width.toString() + "x" + canvas.height)
+                }
+                showInference(lastProcessingTimeMs.toString() + "ms")
+            }
+        }
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.tfe_od_camera_connection_fragment_tracking
+    }
+
+    override fun getDesiredPreviewFrameSize(): Size {
+        return DESIRED_PREVIEW_SIZE
+    }
+
+    // Which detection model to use: by default uses Tensorflow Object Detection API frozen
+    // checkpoints.
+    private enum class DetectorMode {
+        TF_OD_API
+    }
+
+    override fun setUseNNAPI(isChecked: Boolean) {
+        runInBackground {
+            try {
+                detector!!.setUseNNAPI(isChecked)
+            } catch (e: UnsupportedOperationException) {
+                LOGGER.e(e, "Failed to set \"Use NNAPI\".")
+                runOnUiThread { Toast.makeText(this, e.message, Toast.LENGTH_LONG).show() }
+            }
+        }
+    }
+
+    override fun setNumThreads(numThreads: Int) {
+        runInBackground { detector!!.setNumThreads(numThreads) }
+    }
+
+    companion object {
+        private val LOGGER = Logger()
+
+        // Configuration values for the prepackaged SSD model.
+        private const val TF_OD_API_INPUT_SIZE = 300
+        private const val TF_OD_API_IS_QUANTIZED = true
+        private const val TF_OD_API_MODEL_FILE = "detect.tflite"
+        private const val TF_OD_API_LABELS_FILE = "labelmap.txt"
+        private val MODE = DetectorMode.TF_OD_API
+
+        // Minimum detection confidence to track a detection.
+        private const val MINIMUM_CONFIDENCE_TF_OD_API = 0.5f
+        private const val MAINTAIN_ASPECT = false
+        private val DESIRED_PREVIEW_SIZE = Size(640, 480)
+        private const val SAVE_PREVIEW_BITMAP = false
+        private const val TEXT_SIZE_DIP = 10f
+    }
+}
