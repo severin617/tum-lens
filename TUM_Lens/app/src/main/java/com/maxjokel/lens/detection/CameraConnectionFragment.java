f37a3238f28b5f806b4baec9c81f7b7e24a7cf8c
David Drews <david@drews.io>
2021-06-09 08:53:09 +0200

    Convert object detection Java classes to Kotlin

--- TUM_Lens/app/src/main/java/com/maxjokel/lens/detection/CameraConnectionFragment.java
+++ TUM_Lens/app/src/main/java/com/maxjokel/lens/detection/CameraConnectionFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.maxjokel.lens.obj_detection;
+package com.maxjokel.lens.detection;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
